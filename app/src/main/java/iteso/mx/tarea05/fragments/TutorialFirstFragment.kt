5787ff69a0a640b5355e83e967222011a5f65999
Ian27Diaz <anand_ian@hotmail.com>
2019-09-20 08:36:30 -0500

    First push

---
+++ app/src/main/java/iteso/mx/tarea05/fragments/TutorialFirstFragment.kt
@@ -0,0 +1,19 @@
+package iteso.mx.tarea05.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import iteso.mx.tarea05.R
+
+class TutorialFirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.tutorial_first_fragment, container, false)
+        return view
+    }
+}
